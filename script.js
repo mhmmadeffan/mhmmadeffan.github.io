cce3e07513e5c843973ac874a3cda6ae4cec890b
mhmmadeffan <82354518+mhmmadeffan@users.noreply.github.com>
2023-08-07 13:48:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student Amikom"],
+        typeSpeed: 100,
+        backSpeed: 200,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
